cdb386cf67762122bb3c51c1c7d9a8dac37ba244
Kevinth-Garcia <itskagz1620@gmail.com>
2025-12-15 21:38:35 -0500

    Actualizacion de frontend Frikimundo : Se ha agregado rutas de carpetas , y conexion de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate, replace } from "react-router-dom";
+import { useAuthStore } from "./store/useAuthStore";
+import { useCartStore } from "./store/useCartStore";
+import AdminRoute from "./components/AdminRoute";
+import Cart from "./components/Cart";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AdminOrders from "./pages/admin/AdminOrders";
+import AdminProducts from "./pages/admin/AdminProducts";
+import AdminUsers from "./pages/admin/AdminUsers";
+import EmailVerification from "./pages/EmailVerification";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import NoEncontrado from "./pages/NoEncontrado";
+import OrderHistory from "./pages/OrderHistory";
+import ProductDetail from "./pages/ProductDetail";
+import Products from "./pages/Products";
+import ResetPassword from "./pages/ResetPassword";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const { user } = useAuthStore();
+  const setUserCart = useCartStore((state) => state.setUser);
+
+  useEffect(() => {
+    setUserCart(user ? user.id : null);
+  }, [user, setUserCart]);
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <div className="flex-grow container mx-auto px-4 py-8"><ToastContainer /></div>
+      
 
-export default App
+
+      <Header />
+      <Cart />
+      
+      <main className="">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+           <Route path="/orders" element={<ProtectedRoute><OrderHistory/></ProtectedRoute> } />
+          <Route path="/admin" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
+          <Route path="index" element={<Navigate to="/admin/products" replace />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="users" element={<AdminUsers />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/verify-email" element={<EmailVerification />} />
+          <Route path="*" element={<NoEncontrado/>}/>
+          
+         
+        </Routes>
+      </main>
+
+      
+      <CartDrawer />
+
+      <Footer />
+    </Router>
+  );
+}
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+// HomePage.jsx
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenido a mi Página de Inicio</h1>
+      <p>Esta es la página principal de mi aplicación React.</p>
+    </div>
+  );
+}
+
+export default Home;
